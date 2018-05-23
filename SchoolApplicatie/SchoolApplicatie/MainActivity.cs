35e1f74049c9c9a51763c4daab0bbd9c2e3f5095
sergenaka <sergenhaydar@gmail.com>
2018-05-23 13:48:41 +0200

    .

---
+++ SchoolApplicatie/SchoolApplicatie/MainActivity.cs
@@ -0,0 +1,63 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Timers;
+using System;
+
+namespace SchoolApplicatie
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button stopknop;
+        private TextView txtCountdown;
+        private int count = 1;
+        Timer timer;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            stopknop = FindViewById <Button> (Resource.Id.stopknop);
+            txtCountdown = FindViewById <TextView> (Resource.Id.txtCountdown);
+            stopknop.Click += stopknop_Click;           
+        }
+
+        private void stopknop_Click(object sender, EventArgs e)
+        {
+            txtCountdown.Text = "Stop";
+            timer.Stop();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            timer = new Timer();
+            timer.Interval = 1000; // 1 sec
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (count < 10)
+            {
+                count++;
+                RunOnUiThread(() =>
+               {
+                   txtCountdown.Text = "" + count;
+               });
+            }
+            else
+            {
+                count = 1;
+                Toast.MakeText(this, "Hello", ToastLength.Short).Show();
+                txtCountdown.Text = "" + count;
+            }
+        }
+    }
+
+}
+
---
+++ Schoolapplicatie2/Schoolapplicatie2/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Schoolapplicatie2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+        private static long START_TIME_IN_MILLIS = 600000;
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
+
